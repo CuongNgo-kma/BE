34a3539210dbb8b18a250443cbc1119a5ad856a1
CuongNgo-kma <ngongocbiencuong@gmail.com>
2023-05-31 19:29:06 +0700

    done

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const cookieParse = require('cookie-parser')
-const { json } = require('express')
-const auth = require('./middleware/auth') 
-const authAdmin = require('./middleware/authAdmin')
-require('dotenv').config()
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const cookieParse = require("cookie-parser");
+const { json } = require("express");
+const auth = require("./middleware/auth");
+const authAdmin = require("./middleware/authAdmin");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 // app.set('view engine', 'ejs');
-app.use(express.json())
-app.use(cookieParse())
-app.use(cors())
-app.use(fileUpload({
-    useTempFiles: true
-}))
-
+app.use(express.json());
+app.use(cookieParse());
+app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 
 //Routes
-app.use('/user', require('./routes/userRouter')) // sử dụng /user tương ứng trỏ đường dẫn /routes/userRouter
-app.use('/api', require('./routes/categoryRouter')) // 
-app.use('/api', require('./routes/upload')) // 
-app.use('/api', require('./routes/productRouter')) // 
-app.use('/api', require('./routes/paymentRouter')) // 
+app.use("/user", require("./routes/userRouter")); // sử dụng /user tương ứng trỏ đường dẫn /routes/userRouter
+app.use("/api", require("./routes/categoryRouter")); //
+app.use("/api", require("./routes/upload")); //
+app.use("/api", require("./routes/productRouter")); //
+app.use("/api", require("./routes/paymentRouter")); //
 
 // app.get("/success", (req, res) => {
 //     res.render("success")
@@ -35,19 +36,23 @@ app.use('/api', require('./routes/paymentRouter')) //
 
 //connect to mongodb
 const URI = process.env.MONGODB_URL;
-mongoose.connect(URI, {
+mongoose.connect(
+  URI,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}, err => {
+    useUnifiedTopology: true,
+  },
+  (err) => {
     if (err) throw err;
-    console.log("Connected to MONGODB")
-})
+    console.log("Connected to MONGODB");
+  }
+);
 
-app.get('/', (req, res) => {
-    res.json({ msg: "Welcome to you" })
-})
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to you" });
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log("server in running on port", PORT)
-})
+  console.log("server in running on port", PORT);
+});
