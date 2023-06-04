201d5eb233db9fc898a9e2336cfbf766d9813bf3
CuongNgo-kma <ngongocbiencuong@gmail.com>
2023-06-05 01:41:03 +0700

    .

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -58,7 +58,9 @@ const userCtrl = {
       const refreshtoken = createRefreshToken({ id: user._id });
 
       res.cookie("refreshtoken", refreshtoken, {
-        httpOnly: true,
+        httpOnly: false,
+        secure: false,
+        sameSite: 'none',
         path: "/user/refresh_token",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
       });
